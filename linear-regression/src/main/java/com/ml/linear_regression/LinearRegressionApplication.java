9dafbeef561cf9cf4b2d10294259dfeb5d49f8ec
BigO_Mojo <nishantgsingh07@gmail.com>
2025-03-23 02:08:02 +0530

    linear-regression initial commit

---
+++ linear-regression/src/main/java/com/ml/linear_regression/LinearRegressionApplication.java
@@ -0,0 +1,13 @@
+package com.ml.linear_regression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinearRegressionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinearRegressionApplication.class, args);
+	}
+
+}
---
+++ linear-regression/src/test/java/com/ml/linear_regression/LinearRegressionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.linear_regression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinearRegressionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
